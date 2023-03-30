ff8b158ce34ef1c2597b0e5eea4f9dbd7e2ada9a
logan <loganparker0803@gmail.com>
2023-03-30 15:12:42 -0500

    bit of spring cleaning

---
+++ src/GameCollection.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class GameCollection {
+
+    ArrayList<Game> list = new ArrayList<Game>();
+    private String listname;
+
+    /* Constructor */
+    public GameCollection(String name) {
+        listname = name;
+    }
+
+    /* Add a game to a GameCollection */
+    public void addGame(Game g) {
+        list.add(g);
+    }
+
+    /* Remove a game from a game collection */
+    public void removeGame(Game g) {
+        int gameIndex = -1;
+        for (Game items : list) {
+            if (list.contains(g)) {
+                gameIndex = list.indexOf(g);
+            }
+        }
+
+        list.remove(gameIndex);
+    }
+
+    /* Get a game from a game collection */
+    public Game getGame(Game g) {
+        int gameIndex = -1;
+        for (Game items : list) {
+            if (list.contains(g)) {
+                gameIndex = list.indexOf(g);
+            }
+        }
+
+        return list.get(gameIndex);
+    }
+
+}
