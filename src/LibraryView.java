0e4d9bc2dc9fa1e6817817ce305b6fc3ed2fbdf5
Charbuscus11 <116667012+Charbuscus11@users.noreply.github.com>
2023-04-04 16:54:19 -0500

    Update LibraryView.java

--- src/LibraryView.java
+++ src/LibraryView.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class Library {
+public class LibraryView {
 
     public static int nums = 0;
     public static int xnums = 100;
