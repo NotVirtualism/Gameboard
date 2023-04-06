48760709eeaa36dda8f3a3d9c82f2307436e4c8d
Blair-St <rstaff935@gmail.com>
2023-04-06 17:11:43 -0500

    Created Basic UserProfile Class

--- src/UserProfile.java
+++ src/UserProfile.java
@@ -1,7 +1,51 @@
-import java.io.*;
 import java.util.ArrayList;
 
 public class UserProfile {
+    private String userName;
+    private String password;
+    ArrayList<Review> userReviews = new ArrayList<>();
+    private int reviewCount = 0;
+
+    private Library userLibrary = new Library();
+
+    public UserProfile(){
+        userName = "None";
+        password = "None";
+    }
+
+    public UserProfile(String username1, String password1){
+        userName = username1;
+        password = password1;
+    }
+
+    public void setPassword(String password1){
+        password = password1;
+    }
+
+    public void setUserName(String userName1){
+        userName = userName1;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void addReview(Review review){
+        userReviews.add(review);
+        reviewCount++;
+    }
+
+    public Review getReview(){
+       return userReviews.get(reviewCount-1);
+    }
+
+    public Library getLibrary(Library library){
+        return library;
+    }
 
 
 }
