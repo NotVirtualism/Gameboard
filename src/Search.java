2c59941a3d05336f16490c0e1cab16c1206e5200
Charbuscus11 <116667012+Charbuscus11@users.noreply.github.com>
2023-04-09 21:51:25 -0500

    Update Search.java

--- src/Search.java
+++ src/Search.java
@@ -1,6 +1,29 @@
 import java.io.*;
 import java.util.ArrayList;
 
-public class Search {
+public class Search extends GameDatabase{
+
+    ArrayList<String> selectedTags = new ArrayList<String>();
+    private GameCollection results;
+    private String searchString;
+    public Search(String text)
+    {
+        searchString = text;
+        selectedTags = new ArrayList<String>();
+    }
+
+    void setTags(ArrayList<String> selectedTags)
+    {
+        int listind = 0;
+        int gameIndex = -1;
+        String multTags[] = {};
+        for (String items : selectedTags) {
+            multTags[listind] = selectedTags.get(listind);
+            listind++;
+        }
+
+        //read through tags list in the Database in getTags
+    }
 
 }
+
