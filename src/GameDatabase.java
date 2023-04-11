a52d35bcddc5e7d1f466d80e06f86896cde7d5e5
Charbuscus11 <116667012+Charbuscus11@users.noreply.github.com>
2023-04-11 14:50:40 -0500

    Update GameDatabase.java

--- src/GameDatabase.java
+++ src/GameDatabase.java
@@ -3,4 +3,15 @@ import java.util.ArrayList;
 
 public class GameDatabase {
 
+    static ArrayList<String> tags = new ArrayList<String>();
+
+    protected static void tags(String tagtxt)
+    {
+        tags.add(tagtxt);
+    }
+
+    public ArrayList<String> getTags()
+    {
+        return tags;
+    }
 }
