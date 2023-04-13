099946aca729c812cc4fdf90411a02858ee91bd5
Blair-St <122568814+Blair-St@users.noreply.github.com>
2023-04-13 14:24:56 -0500

    Added Class Files for Saving Users' Data

---
+++ src/ExportUserProfile.java
@@ -0,0 +1,9 @@
+import org.w3c.dom.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+public class ExportUserProfile {
+    private Document userfile;
+}
